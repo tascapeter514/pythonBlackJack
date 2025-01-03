2eb9556b304cc9d3dc178043778873c8783f1693
petertasca <tascapeter@gmail.com>
2025-01-03 16:09:03 -0500

    shuffled agorithm implemented

--- black_jack.py
+++ black_jack.py
@@ -1,3 +1,5 @@
+import random
+
 values = list(range(52))
 
 
@@ -8,13 +10,43 @@ def create_card(val):
     return card
 
 def display_card(c):
-    print(c)
     split = list(c)
-    print(split)
     suitHash = {'S': '♤', 'H': '♡', 'D': '♦', 'C': '♧'}
     suit = suitHash[split[1]]
     card = str(split[0]) + suit
     return card
 
-ace_spade = create_card(12)
-print(display_card(ace_spade))
+ace_spade = create_card(45)
+# print(display_card(ace_spade))
+
+deck = [create_card(x) for x in range(52)]
+
+def shuffle_deck(d):
+    print(d)
+    count = len(d)
+    print(count)
+    for i in range(count - 1, 0, -1):
+        j = random.randint(0, i)
+        d[i], d[j] = d[j], d[i]
+    return d
+
+    
+
+
+# def shuffle_deck(d):
+#     count = len(d)
+#     for val in d:
+#         random_index = int(count * random.random())
+#         temp = val
+#         val = d[random_index]
+#         d[random_index] = temp
+#     print(len(d))
+#     return d
+
+
+
+print(shuffle_deck(deck))
+
+
+    
+
