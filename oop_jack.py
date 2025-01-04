116e7fc00730ba1f6f27f55e1341ceff8900c8ec
petertasca <tascapeter@gmail.com>
2025-01-04 16:03:06 -0500

    deal and shuffled methods implemented

--- oop_jack.py
+++ oop_jack.py
@@ -1,41 +1,42 @@
-
-# suits = ['S', 'H', 'D', 'C']
-# faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
-# values = [x for x in range(52)]
 import random
 class Deck:
 
-    def __init__(self):
-        self.suits = ['S', 'H', 'D', 'C']
-        self.faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
-        self.values = [x for x in range(52)]
-        self.deck = [str(self.faces[int(x % 13)]) + str(self.suits[int(x / 13)]) for x in range(52)]
+    def __init__(self, suits = ['S', 'H', 'D', 'C'], faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'], values = [x for x in range(52)]):
+        self._deck = [str(faces[int(x % 13)]) + str(suits[int(x / 13)]) for x in range(52)]
+
 
     def shuffle(self):
-        count = len(self.deck)
-        for i in range(count):
+        for i in range(len(self._deck)):
             j = random.randint(0, i)
-            self.deck[i], self.deck[j] = self.deck[j], self.deck[i]
-        return self.deck
+            self._deck[i], self._deck[j] = self._deck[j], self._deck[i]
+        return self._deck
     
+    def deal(self):
+        print(self._deck)
+        hand = [self._deck.pop() for i in range(1)]
+        return hand
+
+class Player:
+    def __init__(self, name, hand):
+        self.name = name
+        self.hand = hand
     
+    def get_count(self):
+        faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+        for card in self.hand:
+
+        
+        
+        
+
+
+
     
-    
-            
-            
+
+deck = Deck()
+# deck = deck.shuffle()
+deck.shuffle()
+print(deck.deal())
+print(deck.deal())
 
 
-
-new_deck = Deck()
-test_deck = new_deck.shuffle()
-print(test_deck)
-
-
-
-
-# print(new_deck.shuffle())
-# new_deck = new_deck.create()
-
-
-
-# print(ace_spade.create(1))
