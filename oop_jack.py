29545e8c0a9a99b285fd05924641d740e351ba83
petertasca <tascapeter@gmail.com>
2025-01-12 14:49:05 -0500

    loser, winner, bust, outcome refactored -- while loop to be removed

--- oop_jack.py
+++ oop_jack.py
@@ -13,25 +13,6 @@ class Card:
         self.suit = self.suits[int(value/ 13)]
         self.face = str(self.faces[int(value % 13)])
 
-
-    # REFACTOR SO IT DISPLAYS AND DOESN'T RETURN
-    # def convert(self):
-    #     suits = {'S': '♤', 'H': '♡', 'D': '♦', 'C': '♧'}
-    #     self.display = self.face + suits[self.suit]
-    #     return self
-
-
-
-def displayCard(c):
-    suits = {'S': '♤', 'H': '♡', 'D': '♦', 'C': '♧'}
-    return c.face + suits[c.suit]
-
-# card = Card(1)
-# print(displayCard(card))
-
-
-
-
 class Deck:
 
     def __init__(self):
@@ -44,72 +25,32 @@ class Deck:
         print('The deck has been shuffled.')
         return self
                 
-    # CALL THIS IN THE GAME INSTANCE?
     def deal(self):
         return self.deck.pop()
 
+def displayCard(c):
+    suits = {'S': '♤', 'H': '♡', 'D': '♦', 'C': '♧'}
+    return c.face + suits[c.suit]
 
 def displayHand(h):
     h = [displayCard(c) for c in h]
     h = " ".join(h)
-    print(h)
+    return h
 
-card1 = Card(0)
-card2 = Card(1)
-card3 = Card(10)
-hand = [card1, card2, card3]
-displayHand(hand)
 
 
 
 class Player:
 
-    # initial card array passed to each player
     def __init__(self, name):
         self.name = name
         self.hand = []
 
     def draw(self, card):
-        # d = '{} drew a {}'
-        # d = d.format(self.name, card.convert().display)
-        # print(d)
         self.hand.append(card)
 
+      
 
-    # REFACTOR ONCE CONVERT METHOD IS SWITCHED TO DISPLAY
-    def reveal(self):
-        print('parent reveal check')
-        h = [displayCard(c) for c in self.hand]
-        h = "".join(list(map(lambda c: c.display + ' ', self.hand)))
-        print(f"{self.name} has drawn {h}")
-
-    def show(self):
-        s = [c.convert() for c in self.hand]
-        s = "".join(list(map(lambda c: c.display + ' ', self.hand)))
-        return s
-
-
-    
-    def hit(self, card):
-        self.hand.append(card)
-
-        
-class Dealer(Player):
-    def reveal(self):
-        i = random.randint(0, 1)
-        c = self.hand[i].convert()
-        print(f"{self.name} has drawn a {c.display} The other card is covered.")
-
-
-    def draw(self, card):
-        self.hand.append(card)
-
-
-# deck = Deck().shuffle()
-# dealer = Dealer('Martin')
-# dealer.draw(deck.deal())
-# dealer.draw(deck.deal())
-# dealer.reveal()
 
 
 #PLAYER AND DEALER COUNT NEEDS TO BE A PROPERTY OF GAME
@@ -121,52 +62,52 @@ class Game:
     #     print("LET'S START SOME BLACKJACK BABY!")
 
     # number_of_players = int(input('How many players would you like to begin with?'))
-    count = {}
-
-
+    
     def __init__(self):
         p1 = input("Please enter your name to begin: \n")
         d = 'Martin Heidegger'
         self.player = Player(p1)
-        self.dealer = Dealer(d)
+        self.dealer = Player(d)
         self.players = [self.dealer, self.player]
+        self.count = {}
 
-
-
-# wind and bust need to be refactored
-# the player is being called both as winner and loser
-# loser.reveal() and winner.reveal() are returning None
-# winner and loser are returning the same variable, not alternate variables
-    def win(self):
-        
-        if self.count[self.player.name] == self.count[self.dealer.name]:
-            d = "It's a draw. {} has {} and {} has {}"
-            d = d.format(self.player.name, self.player.show(), self.dealer.name, self.dealer.show())
-        else: 
-            winner = self.player if self.count[self.player.name] > self.count[self.dealer.name] else self.dealer
-            loser = self.player if self.count[self.player.name] < self.count[self.dealer.name] else self.dealer
-            # print(f"The count is {self.count}. The winner is {winner.name} and the loser is {loser.name}")
-            w = '{} has won this round with {}'
-            w = w.format(winner.name, winner.show())
-            print(w)
-
-    def bust(self):
-        print('bust check')
-        loser = self.player if self.count[self.player.name] > self.count[self.dealer.name] else self.dealer
-        winner = self.player if self.count[self.player.name] < self.count[self.dealer.name] else self.dealer
-        # print(f"The count is {self.count}. The loser is {loser.name} and the winner is {winner.name}")
-        l = '{} has gone bust with {}. {} wins with {}'
-        l = l.format(loser.name, loser.show(), winner.name, winner.show())
-        print(l)
-    
     def set_count(self):
-            for player in self.players:
-                val = sum([int(c.face) if c.face not in 'JQKA' else 10 for c in player.hand])
-                self.count[player.name] = val
+        for player in self.players:
+            val = sum([int(c.face) if c.face not in 'JQKA' else 10 for c in player.hand])
+            self.count[player.name] = val
+        print("set count:", self.count)
     
     def get_count(self, p):
         val = sum([int(c.face) if c.face not in 'JQKA' else 10 for c in p.hand])
         self.count[p.name] = val
+
+    # def winner(self):
+    #     print('winner count:', self.count)
+    #     w = self.player if self.count[self.player.name] < 22 and self.count[self.player.name] > self.count[self.dealer.name] else self.dealer
+    #     print('winner:', w.name)
+    #     return w
+        
+    def loser(self):
+        if any(c > 22 for c in self.count.values()):
+            return self.player if self.count[self.player.name] > self.count[self.dealer.name] else self.dealer
+        elif all (c < 22 for c in self.count.values()):
+            return self.player if self.count[self.player.name] < self.count[self.dealer.name] else self.dealer
+
+    def winner(self):
+        if any(c > 22 for c in self.count.values()):
+            return self.player if self.count[self.player.name] < self.count[self.dealer.name] else self.dealer
+        elif all (c < 22 for c in self.count.values()):
+            return self.player if self.count[self.player.name] > self.count[self.dealer.name] else self.dealer
+
+    def outcome(self):
+        print('outcome function check')
+        if self.count[self.player.name] == self.count[self.dealer.name]:
+            print(f"It's a draw. {self.player.name} has {displayHand(self.player.hand)} and {self.dealer.name} has {displayHand(self.dealer.hand)}")
+        else:
+            print(f"{self.winner().name} has won this round with {displayHand(self.winner().hand)}. {self.loser().name} has lost with {displayHand(self.loser().hand)}") 
+
+    def bust(self):
+        print(f"{self.loser().name} has gone bust with {displayHand(self.loser().hand)}. {self.winner().name} wins with {displayHand(self.winner().hand)}")
 
     def replay(self):
         replay = input("Do you wish to play again?\n")
@@ -179,7 +120,6 @@ class Game:
             
 
     def play_game(self):
-        # print(f'Welcome. You are facing the notorious {self.dealer.name}')
         print(f"Welcome. Your dealer, {self.dealer.name}, is quite drunk. You sidle up to the table, ready to take advantage of his bibulous state.")
         print('The deck is about to be shuffled.')
         deck = Deck().shuffle()
@@ -187,30 +127,39 @@ class Game:
             for p in self.players:
                 p.draw(deck.deal())
         for p in self.players:
-            p.reveal()
+            if p.name == 'Martin Heidegger':
+                print(f"{p.name} has drawn a {displayCard(p.hand[1])}")
+            else:
+                print(f"{p.name} has drawn a {displayHand(p.hand)}")
         self.set_count()
         while all(c < 22 for c in self.count.values()):
             choice = input("Would you like to hit or stand?\n")
             if choice == 'hit':
-                self.player.hit(deck.deal())
-                self.player.reveal()
+                self.player.draw(deck.deal())
+                print(f"{self.player.name} has drawn a {displayCard(self.player.hand[len(self.player.hand) - 1])}")
                 self.get_count(self.player)
+                print(self.count)
             elif choice == 'stand':
                 while self.count[self.dealer.name] < 17:
-                    self.dealer.hit(deck.deal())
+                    self.dealer.draw(deck.deal())
+                    print(f"{self.dealer.name} has drawn a {displayCard(self.dealer.hand[len(self.dealer.hand) - 1])}")
                     self.get_count(self.dealer)
+                    print(self.count)
                 if self.count[self.dealer.name] < 22:
-                    for player in self.players:
-                        player.show()
-                    self.win()
-                    self.replay() 
+                    print('inner outcome check')
+                    self.outcome()
+                    break
+                self.replay()
+        print('inner outcome check')             
         self.bust()
-        # self.win()
         self.replay()
+
+
         
 
-# game = Game()
-# game.play_game()
+game = Game()
+game.play_game()
+
         
 
 
